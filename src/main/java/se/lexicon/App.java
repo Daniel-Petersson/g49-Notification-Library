dbba1e0379d560bd9bea79830e1264f17788f4c4
binleiden <daniel.petersson83@gmail.com>
2024-04-23 19:21:40 +0200

    Implemented singleton design pattern

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -12,10 +12,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        EmailDao dao = EmailDaoImpl.getInstance();
-        Email email1 = dao.save(new Email("test","TestSub","testcontent"));
-        System.out.println(email1);
-        Email foundEmail = dao.find(email1.getId());
-        System.out.println(foundEmail.summary());
+
     }
 }
